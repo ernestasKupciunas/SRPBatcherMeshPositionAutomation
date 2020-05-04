c95156a5fbe58e1875968b01c2a1bcde6d86c91d
ernestasKup <ernestas.kupciunas@gmail.com>
2020-05-04 11:00:03 +0300

    Project upload. Added  .gitignore and LFS

---
+++ SRPBatcherMeshPosition/Assets/Scripts/CheckObjectPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckObjectPosition : MonoBehaviour
+{
+    [SerializeField]
+    float printTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintPosition(printTime));
+       
+    }
+
+    IEnumerator PrintPosition(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Debug.Log("GameObject " + gameObject.name.ToString() + " position: " + gameObject.GetComponent<MeshRenderer>().transform.position);
+    }
+}
---
+++ SRPBatcherMeshPosition/Assets/Tests/Runtime/UniversalGraphicsTests.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.XR;
+using UnityEngine.TestTools.Graphics;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class UniversalGraphicsTests
+{
+#if UNITY_ANDROID
+    static bool wasFirstSceneRan = false;
+    const int firstSceneAdditionalFrames = 3;
+#endif
+    public const string universalPackagePath = "Assets/ReferenceImages";
+
+    [UnityTest, Category("UniversalRP")]
+    [PrebuildSetup("SetupGraphicsTestCases")]
+    [UseGraphicsTestCases(universalPackagePath)]
+
+
+    public IEnumerator Run(GraphicsTestCase testCase)
+    {
+        SceneManager.LoadScene(testCase.ScenePath);
+
+        // Always wait one frame for scene load
+        yield return null;
+
+        var cameras = GameObject.FindGameObjectsWithTag("MainCamera").Select(x=>x.GetComponent<Camera>());
+        var settings = Object.FindObjectOfType<UniversalGraphicsTestSettings>();
+        Assert.IsNotNull(settings, "Invalid test scene, couldn't find UniversalGraphicsTestSettings");
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        yield return null;
+
+        for (int i = 0; i < settings.WaitFrames; i++)
+            yield return null;
+
+#if UNITY_ANDROID
+        // On Android first scene often needs a bit more frames to load all the assets
+        // otherwise the screenshot is just a black screen
+        if (!wasFirstSceneRan)
+        {
+            for(int i = 0; i < firstSceneAdditionalFrames; i++)
+            {
+                yield return null;
+            }
+            wasFirstSceneRan = true;
+        }
+#endif
+
+        ImageAssert.AreEqual(testCase.ReferenceImage, cameras.Where(x => x != null), settings.ImageComparisonSettings);
+
+        // Does it allocate memory when it renders what's on the main camera?
+        bool allocatesMemory = false;
+        var mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        // 2D Renderer is currently allocating memory, skip it as it will always fail GC alloc tests.
+        var additionalCameraData = mainCamera.GetUniversalAdditionalCameraData();
+        bool is2DRenderer = additionalCameraData.scriptableRenderer is Renderer2D;
+        
+        if (!is2DRenderer)
+        {
+            try
+            {
+                ImageAssert.AllocatesMemory(mainCamera, settings?.ImageComparisonSettings);
+            }
+            catch (AssertionException)
+            {
+                allocatesMemory = true;
+            }
+            if (allocatesMemory)
+                Assert.Fail("Allocated memory when rendering what is on main camera");
+        }
+    }
+
+#if UNITY_EDITOR
+    [TearDown]
+    public void DumpImagesInEditor()
+    {
+        UnityEditor.TestTools.Graphics.ResultsUtility.ExtractImagesFromTestProperties(TestContext.CurrentContext.Test);
+    }
+#endif
+}
